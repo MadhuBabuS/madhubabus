1fb626194daf307e18ccd0024b118215e4ce80e0
mbabu <madhu.babu@armor.com>
2017-01-12 15:08:15 +0530

    Initial commit

---
+++ madhubabus/main.js
@@ -0,0 +1,14 @@
+﻿
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+//alert('test');
+
+/*document.querySelector('html').onclick=function() {
+    alert('clicked');
+}*/
+
+document.querySelector('button').onclick=function() {
+    var name = prompt('clicked');
+    alert(name);
+}
